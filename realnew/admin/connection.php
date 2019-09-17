0858cc102b70e7d8483513f689eb385daf685f39
samarth0174 <38130850+samarth0174@users.noreply.github.com>
2019-09-17 19:24:27 +0530

    init

---
+++ realnew/admin/connection.php
@@ -0,0 +1,8 @@
+<?php
+
+//database connection information
+$host="localhost:3306";
+$user="root";
+$password="";
+$db=mysqli_connect("localhost:3306","root","","property") or die(mysql_error());
+?>
